a5e3d11063b980ccaa31e295e09d57be59212db2
SamanyuKamra <108547482+SamanyuKamra@users.noreply.github.com>
2022-12-20 20:41:11 +0530

    Update test.c

--- test.c
+++ test.c
@@ -5,72 +5,65 @@
 #include<unistd.h>
 
 sem_t forks[5];
-sem_t sauce_bowl;
-
-int no_of_sauce_bowls = 4;
-
-void print_sauce_bowl(){
-    //printf("There are %d sauce bowls remaining\n",no_of_sauce_bowls);
-}
-
-void eat(int phil_no){
-    printf("Philosopher %d is eating.\n",phil_no);
+sem_t sauce;
+int bowls = 2;
+void eat(int phi){
+    printf("Philosopher %d is eating.\n",phi);
     sleep(4);
-    printf("Philosopher %d has finished eating.\n",phil_no);
+    printf("Philosopher %d has finished eating.\n",phi);
+
 }
+void * getPhil(void * n){
+	int phi=*(int *)n;
 
-void * philosopher(void * num){
-	int phil_no=*(int *)num;
+    int f1, f2;
 
-    int fork_1, fork_2;
-
-    if (phil_no == 4){
-        fork_1 = 0;
-        fork_2 = 4;
+    if (phi == 4){
+        f1 = 0;
+        f2 = 4;
     }
     else{
-        fork_1 = phil_no;
-        fork_2 = phil_no + 1;
+        f1 = phi;
+        f2 = phi + 1;
     }
 
     int i;
     for (i=0; i<10; i++){
-        sem_wait(&sauce_bowl);
-        printf("Philospher %d has picked up a sauce bowl\n", phil_no);
-        no_of_sauce_bowls--;
-        print_sauce_bowl();
-        sem_wait(&forks[fork_1]);
-        printf("Philosopher %d has picked up fork %d\n",phil_no,fork_1);
-        sem_wait(&forks[fork_2]);
-        printf("Philosopher %d has picked up fork %d\n",phil_no,fork_2);
+        sem_wait(&sauce);
+        printf("Philospher %d has picked up a sauce bowl\n", phi);
+        bowls--;
+        //print_sauce_bowl();
+        sem_wait(&forks[f1]);
+        printf("Philosopher %d has picked up fork %d\n",phi,f1);
+        sem_wait(&forks[f2]);
+        printf("Philosopher %d has picked up fork %d\n",phi,f2);
 
-        eat(phil_no);
+        eat(phi);
 
-        sem_post(&forks[fork_2]);
-        printf("Philosopher %d has put down fork %d\n",phil_no,fork_2);
-        sem_post(&forks[fork_1]);
-        printf("Philosopher %d has put down fork %d\n",phil_no,fork_1);
-        sem_post(&sauce_bowl);
-        printf("Philospher %d has put down a sauce bowl\n", phil_no);
-        no_of_sauce_bowls++;
-        print_sauce_bowl();
+        sem_post(&forks[f2]);
+        printf("Philosopher %d has put down fork %d\n",phi,f2);
+        sem_post(&forks[f1]);
+        printf("Philosopher %d has put down fork %d\n",phi,f1);
+        sem_post(&sauce);
+        printf("Philospher %d has put down a sauce bowl\n", phi);
+        bowls++;
+        //print_sauce_bowl();
     }
-    printf("Philosopher %d is full.\n",phil_no);
+    printf("Philosopher %d is full.\n",phi);
 }
-
 int main()
 {
-	int i,a[5];
+	int i,arr[5];
 	pthread_t t_id[5];
 
-    sem_init(&sauce_bowl, 0, 4);
+    sem_init(&sauce, 0, 2);
 	
 	for(i=0;i<5;i++){
 		sem_init(&forks[i],0,1);
     }
 		
 	for(i=0;i<5;i++){
-		pthread_create(&t_id[i],NULL,philosopher,(void *)&i);
+		pthread_create(&t_id[i],NULL,getPhil,(void *)&i);
 	}
 
 	for(i=0;i<5;i++){
