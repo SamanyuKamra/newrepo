1de49475f29165ea6abd61214e695c83170e9f7f
SamanyuKamra <108547482+SamanyuKamra@users.noreply.github.com>
2022-12-20 19:36:23 +0530

    Update test1.c

--- test1.c
+++ test1.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -6,39 +13,61 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main()
-{
-    int fd1;
+int main(){
+    int file_add;
+    
+    char fifo_add[20] = "helloo";
 
-    char * myfifo = "/tmp/myfifo";
+    while(1){
+        char inp[2000];
+        file_add = open(fifo_add, O_RDONLY);
+        read(file_add, inp, 2000);
+        close(file_add);
 
-    mkfifo(myfifo, 0666);
+        //Printing stuff
+        int counter;
 
-    char strings[100], send[10];
-    int curr = 1;
+        char char_id[5];
+        int curr_index;
 
-    while (curr < 50)
-    {
+        if (inp[0]=='1'){
+            curr_index = inp[1] - '0';
+            counter = 2;
+        }
+        else if (inp[0]=='2'){
+            curr_index = 10*(inp[1] - '0') + (inp[2] - '0');
+            counter = 3;
+        }
+
+        printf("P2: Recieved current index %i\n", curr_index);
         
-        fd1 = open(myfifo,O_RDONLY);
-        read(fd1, strings , 100);
+        int i, j;
+        for  (i = 0; i<5; i++){
+            printf("P2: Index %d: ",curr_index);
+            for (j = 0; j<4; j++){
+                printf("%c",inp[counter]);
+                counter++;
+            }
+            printf("\n");
+            curr_index++;
+        }
 
-        printf("\nStrings received from P1\n%s", strings);
-        close(fd1);
+        curr_index--;
 
-        curr += 5;
-        char ind[5];
-        sprintf(ind,"%d",curr);
-	    strcpy(send,ind);
+        char new_id[10];
+        sprintf(new_id, "%d", curr_index-1);
 
-        printf("Highest index has been sent back to P1\n");
+        file_add = open(fifo_add, O_WRONLY);
+        write(file_add, new_id, strlen(new_id)+1);
+        close(file_add);
 
-        fd1 = open(myfifo,O_WRONLY);
-        write(fd1, send, strlen(send)+1);
-        close(fd1);
+        printf("P2: Sent current index %i\n", curr_index);
 
+        if (curr_index>48){
+            break;
+        }
+        
     }
 
     return 0;
-    
 }
