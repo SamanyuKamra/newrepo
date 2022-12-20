e10c1ea6444bbdb93e82fb3a57d16c94cd932595
SamanyuKamra <108547482+SamanyuKamra@users.noreply.github.com>
2022-12-20 17:02:39 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,57 +1,81 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <semaphore.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+
+sem_t sauce[2];
 sem_t forks[5];
 pthread_mutex_t waiter;
-void getForks(int philosopherArrived)
-{
-    sem_wait(&forks[philosopherArrived]);
-    sem_wait(&forks[(philosopherArrived + 1) % 5]);
+
+int getBowl(){
+    int x = 0;
+    while(i<2){
+        int semTemp = -1;
+        int error = sem_getvalue(&sauce[x] , &semTemp);
+        if(semTemp == 1)
+        {
+            sem_wait(&sauce[x]);
+            return (x);
+        }
+        x++;
+    }
+
 }
-void eat(int philosopherArrived)
+
+void eat(int philosphers, int bowls)
 {
-    printf("Philosopher %d eating with forks %d and %d\n", philosopherArrived,
-           philosopherArrived, (philosopherArrived + 1) % 5);
+    printf("Philosopher %d eating in Bowl %d with forks %d and %d\n", philosphers,bowls, philosphers, (philosphers + 1) % 5);
     usleep(500000);
-    sem_post(&forks[philosopherArrived]);
-    sem_post(&forks[(philosopherArrived + 1) % 5]);
+    sem_post(&forks[philosphers]);
+    sem_post(&forks[(philosphers + 1) % 5]);
+    sem_post(&sauce[bowls]);
+
 }
-void *philosopherAction(void *philosopherIndex)
+void getforks(int philosphers)
 {
-    int philosopherArrived = *((int *)philosopherIndex);
-    while (1)
+    sem_wait(&fork[philosphers]);
+    sem_wait(&forks[(philosphers + 1) % 5]);
+
+}
+void *philospherThinking(void *i)
+{
+    int philosphers = *((int *)i);
+    while(true)
     {
         pthread_mutex_lock(&waiter);
-        getForks(philosopherArrived);
+        getforks(philosphers);
+        int bowls = getBowl();
         pthread_mutex_unlock(&waiter);
-        eat(philosopherArrived);
+        eat(philosphers, bowls);
+
     }
 }
-int main()
-{
-    pthread_t philosopher[5];
-    int philosopherIndex[5] ={0,1,2,3,4};
+int main(){
+    pthread_t DP[5];
+    int i[5] = {0,1,2,3,4};
     pthread_mutex_init(&waiter, NULL);
-    //creating forks
-    for (int i = 0; i < 5; i++)
+    int j = 0;
+    while(j<5) //Forks
     {
-        sem_init(&forks[i], 0, 1);
+        sem_init(&forks[j],0,1);
+        j++;
     }
-    //creating philosophers as threads
-    for (int i = 0; i < 5; i++)
+    int k = 0;
+    while (k<2)
     {
-        if (pthread_create(&philosopher[i], NULL, philosopherAction, &philosopherIndex[i]) == -1)
+        sem_init(&sauce[k],0,1);
+    }
+    for (int a = 0; a < 5; a++)
+    {
+        if (pthread_create(&DP[a], NULL, philospherThinking, &i[a]) == -1)
         {
             printf("error creating pthread\n");
         }
     }
-    for (int i = 0; i < 5; i++)
-    {
-
-        pthread_join(philosopher[i], NULL);
+    for(int a = 0; a<5;a++){
+        pthread_join(DP[a], NULL);
     }
-
     return 0;
+
 }
