8354721f20bf09edaad47abfda1e76e79c7f5f14
SamanyuKamra <108547482+SamanyuKamra@users.noreply.github.com>
2022-12-20 19:17:48 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,96 +1,110 @@
-
-#include <unistd.h>
-#include <stdio.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <string.h>
+#include <sys/un.h>
 #include <stdlib.h>
-#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
 
+#define NAME "/tmp/sock"
 
-#define PORT 8080
+struct myData
+{
+    char stringArray[5][5];
+    int indexArray[5];
+    int Index;
+};
 
-int main()
-{	
-	int server_fd, new_socket, valread;
-	struct sockaddr_in address;
-	int opt = 1;
-	
-	char arr[51][10];
+void getCharArrays(int Index, char toBeSent[5][5], char stringArray[50][5], int indexArr[])
+{
+    int j = 0;
+    for (int i = Index; i < Index + 5; i++)
+    {
+        strcpy(toBeSent[j], stringArray[i]);
+        indexArr[j] = i;
+        j++;
+    }
+}
+void printCharArray(char toBeSent[5][5])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s\n", toBeSent[i]);
+    }
+}
+void randomStringGenerator(char stringArray[50][5])
+{
+    srand(time(NULL));
+    for (int i = 0; i < 50; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            stringArray[i][j] = rand() % 26 + 65;
+        }
+    }
+}
+struct myData messageStructure(char stringArray[50][5], char toBeSent[5][5], int indexArray[5], int Index)
+{
+    struct myData data;
+    getCharArrays(Index, toBeSent, stringArray, indexArray);
+    memcpy(data.indexArray, indexArray, sizeof(int) * 5);
+    memcpy(data.stringArray, stringArray, sizeof(char) * 25);
+    return (data);
+}
 
-	for(int i = 1; i<51 ; i++){
-		char s[10] = "string";
-		char no[5];
-		sprintf(no,"%d",i);
-		strcat(s,no);
-		strcpy(arr[i],s);
-	
-	}	
-	
-	int addrlen = sizeof(address);
-	char ind[10] = {0};
-	char send_data[100];
-	
-	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-	{
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
-	
-	// Forcefully attaching socket to the port 8080
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-	{
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons( PORT );
-	
-	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
-	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
+int main(int argc, char const *argv[])
+{
+    char stringArray[50][5] = {{0}};
+    randomStringGenerator(stringArray);
+    char toBeSent[5][5];
+    int indexArr[5];
+    int Index;
+    int sock, msgsock;
+    struct sockaddr_un server;
 
-	if (listen(server_fd, 3) < 0)
-	{
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
+    sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        printf("Socket making failed\n");
+        exit(1);
+    }
+    server.sun_family = AF_UNIX;
+    strcpy(server.sun_path, NAME);
 
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
-	{
-		perror("accept");
-		exit(EXIT_FAILURE);
-	}
+    if (bind(sock, (struct sockaddr *)&server, sizeof(struct sockaddr_un)))
+    {
+        perror("binding stream socket");
+        unlink(NAME);
+        exit(1);
+    }
+    int option = 1;
+    listen(sock, 5);
+    msgsock = accept(sock, 0, 0);
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
+    if (msgsock == -1)
+        printf("Socket error\n");
+    else
+    {
 
-	int curr = 1;
+        struct myData data1 = messageStructure(stringArray, toBeSent, indexArr, 5);
+        write(msgsock, (void *)&data1, 52);
+        int receivedIndex;
+        read(msgsock,&receivedIndex,sizeof(int));
+        printf("The recieved index from P2 is %d\n",receivedIndex);
+        struct myData data2 = messageStructure(stringArray, toBeSent, indexArr, receivedIndex+1);
+        write(msgsock, (void *)&data2, 52);
+        read(msgsock,&receivedIndex,sizeof(int));
+        printf("The recieved index from P2 is %d\n",receivedIndex);
 
-	while(curr<50){
+    }
 
-		strcat(arr[curr],"\n");
+    close(msgsock);
 
-		for(int i = curr+1 ; i<= curr+4 ; i++){
-
-			strcat(arr[curr],arr[i]);
-			strcat(arr[curr],"\n");	
-		}
-
-		strcpy(send_data, arr[curr]);
-		
-		send(new_socket , send_data , strlen(send_data) , 0 );
-
-		printf("Stirngs sent form p1\n");
-
-		valread = read( new_socket , ind, 10);
-		curr = atoi(ind);
-			
-		printf("Highest index returned form p2:-%d\n", curr);
-
-	}
-
-	return 0;
-
+    unlink(NAME);
+    close(sock);
+    return (0);
 }
