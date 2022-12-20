df057fc3cf435d6544276053860bb1694b35e7a6
SamanyuKamra <108547482+SamanyuKamra@users.noreply.github.com>
2022-12-20 19:23:25 +0530

    Update test1.c

--- test1.c
+++ test1.c
@@ -1,70 +1,44 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/un.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include <time.h>
-struct myData
+
+int main()
 {
-    char stringArray[5][5];
-    int indexArray[5];
-    int Index;
-};
-void printCharArray(char toBeSent[5][5]){
-    printf("The strings received from P1: \n");
-    for (int i = 0; i < 5; i++)
+    int fd1;
+
+    char * myfifo = "/tmp/myfifo";
+
+    mkfifo(myfifo, 0666);
+
+    char strings[100], send[10];
+    int curr = 1;
+
+    while (curr < 50)
     {
-        printf("%s ",toBeSent[i]);
+        
+        fd1 = open(myfifo,O_RDONLY);
+        read(fd1, strings , 100);
+
+        printf("\nStrings received from P1\n%s", strings);
+        close(fd1);
+
+        curr += 5;
+        char ind[5];
+        sprintf(ind,"%d",curr);
+	    strcpy(send,ind);
+
+        printf("Highest index has been sent back to P1\n");
+
+        fd1 = open(myfifo,O_WRONLY);
+        write(fd1, send, strlen(send)+1);
+        close(fd1);
+
     }
-    printf("\n");
-}
-void printIndexArray(int toBeSent[5]){
-    printf("The string index received from P1: \n");
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d ",toBeSent[i]);
-    }
-    printf("\n");
-}
 
-int main(int argc, char const *argv[])
-{
-    int Index;
-    int sock;
-    struct sockaddr_un server;
-
-    sock = socket(AF_UNIX, SOCK_STREAM, 0);
-
-    if (sock < 0)
-    {
-        perror("opening stream socket");
-        exit(1);
-    }
-    server.sun_family = AF_UNIX;
-    strcpy(server.sun_path, "/tmp/sock");
-
-    if (connect(sock, (struct sockaddr *)&server, sizeof(struct sockaddr_un)) < 0)
-    {
-        close(sock);
-        perror("connecting stream socket");
-        exit(1);
-    }
-    struct myData data1;
-    read(sock,(void*)&data1,52);
-    printCharArray(data1.stringArray);
-    printIndexArray(data1.indexArray);
-    Index = data1.indexArray[4];
-    write(sock,&Index,sizeof(int));
-
-    read(sock,(void*)&data1,52);
-    printCharArray(data1.stringArray);
-    printIndexArray(data1.indexArray);
-    Index = data1.indexArray[4];
-    write(sock,&Index,sizeof(int));
-    close(sock);
-    return (0);
+    return 0;
+    
 }
