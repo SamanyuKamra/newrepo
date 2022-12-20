e5d282559fd21a30a5406fa07d5e2e47c47ae847
SamanyuKamra <108547482+SamanyuKamra@users.noreply.github.com>
2022-12-20 18:06:08 +0530

    Update test1.c

--- test1.c
+++ test1.c
@@ -1,70 +1,53 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/un.h>
+
 #include <stdio.h>
-#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <string.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <time.h>
-struct myData
+#include <unistd.h>
+
+#define PORT 8080
+
+int main()
 {
-    char stringArray[5][5];
-    int indexArray[5];
-    int Index;
-};
-void printCharArray(char toBeSent[5][5]){
-    printf("The strings received from P1: \n");
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%s ",toBeSent[i]);
-    }
-    printf("\n");
-}
-void printIndexArray(int toBeSent[5]){
-    printf("The string index received from P1: \n");
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d ",toBeSent[i]);
-    }
-    printf("\n");
-}
+	int sock = 0, valread;
+	struct sockaddr_in serv_addr;
 
-int main(int argc, char const *argv[])
-{
-    int Index;
-    int sock;
-    struct sockaddr_un server;
+	char strings[1024] = {0};
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		printf("\n Socket creation error \n");
+		return -1;
+	}
 
-    sock = socket(AF_UNIX, SOCK_STREAM, 0);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+	
+	// Convert IPv4 and IPv6 addresses from text to binary form
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+	{
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
+	}
 
-    if (sock < 0)
-    {
-        perror("opening stream socket");
-        exit(1);
-    }
-    server.sun_family = AF_UNIX;
-    strcpy(server.sun_path, "/tmp/sock");
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	{
+		printf("\nConnection Failed \n");
+		return -1;
+	}
 
-    if (connect(sock, (struct sockaddr *)&server, sizeof(struct sockaddr_un)) < 0)
-    {
-        close(sock);
-        perror("connecting stream socket");
-        exit(1);
-    }
-    struct myData data1;
-    read(sock,(void*)&data1,52);
-    printCharArray(data1.stringArray);
-    printIndexArray(data1.indexArray);
-    Index = data1.indexArray[4];
-    write(sock,&Index,sizeof(int));
+	int curr = 1;
+	while(curr < 50){
 
-    read(sock,(void*)&data1,52);
-    printCharArray(data1.stringArray);
-    printIndexArray(data1.indexArray);
-    Index = data1.indexArray[4];
-    write(sock,&Index,sizeof(int));
-    close(sock);
-    return (0);
+		valread = read( sock , strings, 1024);
+		printf("Stirngs received from p1 :- \n");
+		printf("%s\n",strings );
+		curr+=5;
+		char ind[5];
+		sprintf(ind,"%d",curr);
+		send(sock , ind , strlen(ind) , 0 );
+		printf("Highest index sent from p2 to p1\n");
+		
+
+	}
+	return 0;
 }
