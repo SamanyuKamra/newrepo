4b3beddb0c8bff39f139f0f90201442443b9deb8
SamanyuKamra <108547482+SamanyuKamra@users.noreply.github.com>
2022-12-21 17:55:37 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,104 +1,78 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <string.h>
-#include <time.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <signal.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<unistd.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include <semaphore.h>
+
+#define len 9
+#define num 50
+
+
+void acquire(char** sem){
+    while(strcmp(*sem,"wait")!=0){
+    }
+}
+
+void release(char** sem){
+    strcpy(*sem,"");
+}
+
+int min(int a , int b){
+    if(a>b) return b;
+    else return a;
+}
 
 int main(){
-    /*Connect to FIFO*/
-    int file_add;
-    struct timespec a1;
+    srand(time(NULL));
+    char* msg[50];
+    for(int i=0;i<num;i++){
+        msg[i] = (char*)malloc((len)*sizeof(char));
+    }
+    //  generate_strings
+    for(int i=0;i<num;i++){
+        int j;
+        if(i>=10){
+            j=3;
+            msg[i][0] = '0'+i/10;
+            msg[i][1] = '0'+i%10;
+            msg[i][2] =' ';
+        }
+        else {
+            j=2;
+            msg[i][0] = '0' + i;
+            msg[i][1] = ' ';
+        }
 
-    char fifo_add[20] = "helloo";
-    mkfifo(fifo_add, 0666);
-    clock_gettime(CLOCK_REALTIME,&a1);
-    /*To generate an array (of size 50) of strings (of length 4)*/ 
-    int i=0, j=0;
-
-    int r_key;
-	char str_2[5];
-	char arr[50][5];
-
-	for (i = 0; i < 50; i++)
-	{
-		for (j = 0; j < 4; j++)
-		{
-			r_key = rand()%26 + 65;
-			str_2[j] = (char)(r_key);
-		}   
-		str_2[5] = '\0';
-		strcpy(arr[i], str_2);
-	}
-
-    /*Prints the contents of the array*/
-    printf("P1: The contents of the array are\n");
-    for (i = 0; i<50; i++){
-        printf("%s\n", arr[i]);
+        while( (j) <= len-2){
+            msg[i][j] = 65 + rand()%26;
+            j++;
+        }
+        msg[i][len-1]='\0';
     }
 
-    
 
-    /*Using FIFO*/
-    int first_id = 0;
+    char* send=(char*)malloc(sizeof(msg[0]));
+    key_t key = ftok("shmfile",50);
+    int shmid = shmget(key,1024,0666|IPC_CREAT);
 
-    
-    while (1){
-        char str[24]="";
-
-        int flag;
-        if (first_id<10){
-            flag = 1;
+    send = (char*) shmat(shmid,NULL,0);
+    for(int i=0;i<num;){
+        int j=i;
+        while(j<min(i+5,num)){
+            strcpy(send,msg[j]);
+            acquire(&send);
+            j++;
         }
-        else{
-            flag = 2;
-        }
-
-        char char_flag[4];
-        sprintf(char_flag, "%d", flag);
-        strcat(str, char_flag);
-
-        char char_id[4];
-        sprintf(char_id, "%d", first_id);
-        strcat(str, char_id);
-
-        strcat(str, arr[first_id]);
-        strcat(str, arr[first_id + 1]);
-        strcat(str, arr[first_id + 2]);
-        strcat(str, arr[first_id + 3]);
-        strcat(str, arr[first_id + 4]);
-        
-        file_add = open(fifo_add, O_WRONLY);
-        write(file_add, str, sizeof(str));
-        close(file_add);
-        
-        printf("P1: Sent with index %i\n", first_id);
-
-
-
-        char new_id[5];
-        int file_add = open(fifo_add, O_RDONLY);
-        read(file_add, new_id, 3);
-        close(file_add);
-
-        first_id = atoi(new_id);
-        first_id++;
-        printf("P1: Recieved index %i\n", first_id);
-
-        if (first_id>48){
-            break;
-        }
-        first_id++;        
-        
+        i=j;
+        printf("MAX ID received by p1: %d\n",i-1);
     }
-
 
     return 0;
 }
