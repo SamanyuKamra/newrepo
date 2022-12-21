825177abfdd42eb3fb5a2f9051a6250f4f2c8c6d
SamanyuKamra <108547482+SamanyuKamra@users.noreply.github.com>
2022-12-21 17:54:12 +0530

    Update test1.c

--- test1.c
+++ test1.c
@@ -1,78 +1,79 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <string.h>
-#include <time.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<unistd.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include <semaphore.h>
+
+#define len 9
+#define num 50
+
+
+void acquire(char** sem){
+    while(strcmp(*sem,"wait")!=0){
+    }
+}
+
+void release(char** sem){
+    strcpy(*sem,"");
+}
+
+int min(int a , int b){
+    if(a>b) return b;
+    else return a;
+}
 
 int main(){
-    int file_add;
-    struct timespec a2;
-    struct timespec a1;
-    
-    char fifo_add[20] = "helloo";
-
-    while(1){
-        char inp[2000];
-        file_add = open(fifo_add, O_RDONLY);
-        read(file_add, inp, 2000);
-        close(file_add);
-
-        //Printing stuff
-        int counter;
-
-        char char_id[5];
-        int curr_index;
-
-        if (inp[0]=='1'){
-            curr_index = inp[1] - '0';
-            counter = 2;
-        }
-        else if (inp[0]=='2'){
-            curr_index = 10*(inp[1] - '0') + (inp[2] - '0');
-            counter = 3;
-        }
-
-        printf("P2: Recieved current index %i\n", curr_index);
-        
-        int i, j;
-        for  (i = 0; i<5; i++){
-            printf("P2: Index %d: ",curr_index);
-            for (j = 0; j<4; j++){
-                printf("%c",inp[counter]);
-                counter++;
-            }
-            printf("\n");
-            curr_index++;
-        }
-
-        curr_index--;
-
-        char new_id[10];
-        sprintf(new_id, "%d", curr_index-1);
-
-        file_add = open(fifo_add, O_WRONLY);
-        write(file_add, new_id, strlen(new_id)+1);
-        close(file_add);
-
-        printf("P2: Sent current index %i\n", curr_index);
-
-        if (curr_index>48){
-            break;
-        }
-        
+    srand(time(NULL));
+    char* msg[50];
+    for(int i=0;i<num;i++){
+        msg[i] = (char*)malloc((len)*sizeof(char));
     }
-    clock_gettime(CLOCK_REALTIME,&a2);
-    printf("Exexution time = %f\n",fabs(((a2.tv_sec - a1.tv_sec)+(a2.tv_nsec - a1.tv_nsec))/1e9));
+    //  generate_strings
+    for(int i=0;i<num;i++){
+        int j;
+        if(i>=10){
+            j=3;
+            msg[i][0] = '0'+i/10;
+            msg[i][1] = '0'+i%10;
+            msg[i][2] =' ';
+        }
+        else {
+            j=2;
+            msg[i][0] = '0' + i;
+            msg[i][1] = ' ';
+        }
+
+        while( (j) <= len-2){
+            msg[i][j] = 65 + rand()%26;
+            j++;
+        }
+        msg[i][len-1]='\0';
+    }
+
+
+    char* send=(char*)malloc(sizeof(msg[0]));
+    key_t key = ftok("shmfile",50);
+    int shmid = shmget(key,1024,0666|IPC_CREAT);
+
+    send = (char*) shmat(shmid,NULL,0);
+    for(int i=0;i<num;){
+        int j=i;
+        while(j<min(i+5,num)){
+            strcpy(send,msg[j]);
+            acquire(&send);
+            j++;
+        }
+        i=j;
+        printf("MAX ID received by p1: %d\n",i-1);
+    }
 
     return 0;
 }
+
