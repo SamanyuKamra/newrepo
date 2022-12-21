f796bd71f0c7199ced4fcad80ab1cadc529a7a3c
SamanyuKamra <108547482+SamanyuKamra@users.noreply.github.com>
2022-12-21 17:54:48 +0530

    Update test1.c

--- test1.c
+++ test1.c
@@ -11,17 +11,18 @@
 #include<sys/shm.h>
 #include <semaphore.h>
 
-#define len 9
+#define len 8
 #define num 50
+#define semfile "semaphorefile"
 
 
 void acquire(char** sem){
-    while(strcmp(*sem,"wait")!=0){
+    while(strcmp(*sem,"wait")==0){
     }
 }
 
 void release(char** sem){
-    strcpy(*sem,"");
+    strcpy(*sem,"wait");
 }
 
 int min(int a , int b){
@@ -29,51 +30,24 @@ int min(int a , int b){
     else return a;
 }
 
+
 int main(){
-    srand(time(NULL));
-    char* msg[50];
-    for(int i=0;i<num;i++){
-        msg[i] = (char*)malloc((len)*sizeof(char));
-    }
-    //  generate_strings
-    for(int i=0;i<num;i++){
-        int j;
-        if(i>=10){
-            j=3;
-            msg[i][0] = '0'+i/10;
-            msg[i][1] = '0'+i%10;
-            msg[i][2] =' ';
-        }
-        else {
-            j=2;
-            msg[i][0] = '0' + i;
-            msg[i][1] = ' ';
-        }
-
-        while( (j) <= len-2){
-            msg[i][j] = 65 + rand()%26;
-            j++;
-        }
-        msg[i][len-1]='\0';
-    }
-
-
-    char* send=(char*)malloc(sizeof(msg[0]));
+    char* send=(char*)malloc((len)*sizeof(char));
     key_t key = ftok("shmfile",50);
     int shmid = shmget(key,1024,0666|IPC_CREAT);
 
-    send = (char*) shmat(shmid,NULL,0);
-    for(int i=0;i<num;){
-        int j=i;
-        while(j<min(i+5,num)){
-            strcpy(send,msg[j]);
+    send = (char*)shmat(shmid,NULL,0);
+
+    int curr=0;
+    while(curr<num){
+        int i=curr;
+        for(;i<min(curr+5,num);i++){
             acquire(&send);
-            j++;
+            printf("received by p2 : %s\n",send);
+            release(&send);
         }
-        i=j;
-        printf("MAX ID received by p1: %d\n",i-1);
+        curr+=5;
     }
 
     return 0;
 }
-
